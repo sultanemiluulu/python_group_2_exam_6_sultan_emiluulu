90f625f1b1da8e98c905a61fbf50495d806f3641
sultaneu <sultanchoponov@gmail.com>
2019-01-12 13:05:58 +0600

    some fixes

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import User
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='client', verbose_name='User')
-    phone = models.CharField(max_length=50, verbose_name='Phone', blank=True)
+    phone = models.CharField(max_length=50, verbose_name='Phone', blank=True, null=True)
     friend = models.ManyToManyField(User, related_name='friend', verbose_name='Friend',
                                     blank=True)
-    avatar = models.ImageField(verbose_name='Image', blank=True)
+    avatar = models.ImageField(verbose_name='Image', blank=True, null=True)
 
     def __str__(self):
         return self.user.get_full_name()
 
 
 class Post(models.Model):
-    header = models.CharField(max_length=100, verbose_name='Header')
-    text = models.TextField(max_length=2000, verbose_name='Text')
+    header = models.CharField(max_length=100, verbose_name='Header', blank=True, null=True)
+    text = models.TextField(max_length=2000, verbose_name='Text', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='When was created')
     author = models.ForeignKey(User, related_name='post', on_delete=models.PROTECT, verbose_name='Post')
 
