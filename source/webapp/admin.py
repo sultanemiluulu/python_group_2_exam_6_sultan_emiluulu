8adad9b5fcda63d02514664c24285d2d522d6994
sultaneu <sultanchoponov@gmail.com>
2019-01-12 12:44:40 +0600

    admin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from webapp.models import UserInfo, Post
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class ClientInline(admin.StackedInline):
+    model = UserInfo
+    filter_horizontal = ('friend',)
+
+class UserInfoAdmin(UserAdmin):
+    inlines = [ClientInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserInfoAdmin)
+admin.site.register(Post)
