2d40c850d5781024576ad8d260672dfcfe2b3ba0
sultaneu <sultanchoponov@gmail.com>
2019-01-12 12:14:25 +0600

    created models UserInfo, Post migrations

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='client', verbose_name='User')
+    phone = models.CharField(max_length=50, verbose_name='Phone')
+    friend = models.ManyToManyField(User, related_name='friend', verbose_name='Friend',
+                                    blank=True, null=True)
+    avatar = models.ImageField(verbose_name='Image')
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Post(models.Model):
+    header = models.CharField(max_length=100, verbose_name='Header')
+    text = models.TextField(max_length=2000, verbose_name='Text')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='When was created')
+    author = models.ForeignKey(User, related_name='post', on_delete=models.PROTECT, verbose_name='Post')
+
+    def __str__(self):
+        return self.header, self.author, self.created_at
